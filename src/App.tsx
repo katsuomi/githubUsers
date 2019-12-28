3593c87768185f5a3c280ead5187ddade4b2e52e
青木克臣 <mugichak1227@gmail.com>
2019-12-28 16:56:16 +0900

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React, { FC } from "react";
 import { Helmet } from "react-helmet";
-import { Redirect, Route, Switch } from "react-router";
+import {　Redirect, Route, Switch } from "react-router";
 import styled from "styled-components";
 import Home from "./containers/Home";
 
-const title = "Search GitHub User App";
+const title = "Search GitHub Users App";
 
-const HeaderWrapper = styled.div``;
+const HeaderWrapper = styled.div`
+  background-color: #eb3434;
+  min-width: 200px;
+  margin: 20px 20%;
+  padding: 10px 20px;
+`;
 
 const Header = styled.h1`
   text-align: center;
+  color: white;
 `;
 
 const App: FC = () => (
