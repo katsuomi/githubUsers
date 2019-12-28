e5d4d60a3d9585db80fd8f6c2c85652c0a6ca5b3
青木克臣 <mugichak1227@gmail.com>
2019-12-28 16:56:01 +0900

    update Home.tsx

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,8 +1,34 @@
 import React, { FC } from "react";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 import pages from "../pages";
 
-const Home: FC = () => <>aaaaaaa</>;
+const ButtonWrapper = styled.div`
+  text-align: center;
+`;
+
+const Button = styled.button`
+  padding: 10px 20px;
+  background-color: #039dfc;
+  width: 300px;
+  margin-top: 100px;
+  text-align: center;
+  font-size: 1.5rem;
+  font-weight:bold;
+  color: white;
+  border-radius: 5%;
+`;
+
+
+const Home: FC = () => (
+  <>
+    <ButtonWrapper>
+      <Link to={'/'}>
+        <Button>ユーザーを検索する</Button>
+      </Link>
+    </ButtonWrapper>
+  </>
+);
 
 export default Home;
