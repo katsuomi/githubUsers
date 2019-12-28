03b6a732d15631e677191bf0673d5325326313a3
katsuomi <mugichak1227@gmail.com>
2019-12-28 22:21:47 +0900

    update page

--- src/pages.ts
+++ src/pages.ts
@@ -2,6 +2,12 @@ const pages = {
   index: {
     path: "/",
     title: "Search GitHub Users App"
+  },
+  users: {
+    index: {
+      path: "/users",
+      title: "github Users"
+    }
   }
 };
 
