29a2f91f1f6eb2f6c7f933384426162d8b4bafd1
katsuomi <mugichak1227@gmail.com>
2020-01-04 18:02:14 +0900

    update saga

--- src/sagas/github.ts
+++ src/sagas/github.ts
@@ -5,12 +5,10 @@ import { getUsers } from "../actions/github";
 import * as api from "../services/github/api";
 
 function* runGetUsers(action: ReturnType<typeof getUsers.start>) {
-  const { companyName } = action.payload;
-
+  const { userName } = action.payload;
   try {
     const handler = api.getUsersFactory();
-    const users = yield call(handler, companyName);
-
+    const users = yield call(handler, userName);
     yield put(getUsers.succeed(action.payload, { users }));
   } catch (error) {
     yield put(getUsers.fail(action.payload, error));
