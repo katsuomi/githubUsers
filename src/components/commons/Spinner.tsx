24c850438167c0e516668e3f866ec206ad8bf355
katsuomi <mugichak1227@gmail.com>
2020-01-04 18:00:42 +0900

    update spinner

--- src/components/commons/Spinner.tsx
+++ src/components/commons/Spinner.tsx
@@ -1,8 +1,25 @@
 import React, { FC } from "react";
 import Loader from "react-loader-spinner";
+import { Dimmer, Segment } from "semantic-ui-react";
+import styled from "styled-components";
+
+const SegmentWrapper = styled.div`
+  > * > * {
+    justify-content: center;
+  }
+`;
+
+const LoadWrapper = styled.p``;
 
 const Spinner: FC = () => (
-  <Loader type="Puff" color="#00BFFF" height={100} width={100} />
+  <SegmentWrapper>
+    <Segment className="spinner">
+      <Dimmer active inverted>
+        <LoadWrapper>読み込み中...</LoadWrapper>
+        <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+      </Dimmer>
+    </Segment>
+  </SegmentWrapper>
 );
 
 export default Spinner;
