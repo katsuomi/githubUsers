3381d2c7cbd4dc507977c39cead4dd642eb5289e
katsuomi <mugichak1227@gmail.com>
2020-01-04 18:01:28 +0900

    update Users

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import styled from "styled-components";
 import SearchContainer from "../../containers/users/SearchContainer";
+import ListContainer from "../../containers/users/ListContainer";
 import pages from "../../pages";
 import HtmlTitle from "../commons/HtmlTitle";
 
@@ -14,6 +15,7 @@ const Users: FC = () => (
     <HtmlTitle title={title} />
     <UsersWrapper>
       <SearchContainer />
+      <ListContainer />
     </UsersWrapper>
   </>
 );
